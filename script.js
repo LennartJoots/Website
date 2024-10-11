bb76c7c1995cc925ca6a0c6c0a9f1d9498f33c9c
LennartJoots <lennjoots@gmail.com>
2024-10-11 08:56:49 +0300

    Private
    Put else if data is undefined.

--- script.js
+++ script.js
@@ -9,16 +9,33 @@ element.addEventListener("keydown", function (event) {
 
 async function fafafa(Address) {
     const url = "http://ip-api.com/json/" + Address
-    fetch(url)
     const response = await fetch(url)
     const data = await response.json()
     console.log(data)
+    if(data.lon && data.lat && data.lon && data.country && data.city != "undefined"){
+    
     const Long = document.querySelector(".Longitude")
+    Long.innerHTML = "Longitude: ";
     Long.innerHTML +=data.lon;
     const Lati = document.querySelector(".Latitude")
+    Lati.innerHTML = "Latitude: ";
     Lati.innerHTML +=data.lat
     const Count = document.querySelector(".Country")
+    Count.innerHTML = "Country: ";
     Count.innerHTML +=data.country
     const Cit = document.querySelector(".City")
+    Cit.innerHTML = "City: ";
     Cit.innerHTML +=data.city
+    }
+    else{
+        const Long = document.querySelector(".Longitude")
+        Long.innerHTML = "Longitude: Private ";
+        const Lati = document.querySelector(".Latitude")
+        Lati.innerHTML = "Latitude: Private ";
+        const Count = document.querySelector(".Country")
+        Count.innerHTML = "Country: Private ";
+        const Cit = document.querySelector(".City")
+        Cit.innerHTML = "City: Private ";
+
+    }
 }
